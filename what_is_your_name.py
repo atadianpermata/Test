0bc0402f199ed26a6a4b838ac20f8a906b3fca75
ata <kacamatamerah@hotmail.com>
2018-08-19 16:20:57 +0700

    comment what is your name code

---
+++ what_is_your_name.py
@@ -0,0 +1,24 @@
+"""
+nama = input ("what is your name ?")
+helo = 'Hai'
+print (helo,nama)
+
+a = input ("input a number for variable A:")
+print ("Variable A is contain :", a)
+f=a
+print ("variabel F = variable A, it is :", f)
+a = input ("input another number for variable A:")
+print ("variabel A is now contain",a, "and variable F is: ", f)
+"""
+
+#swap variable
+b = input ("input value for B : ")
+c = input ("input value for C : ")
+print ("the value of B is : ", b, ", and the value of C is : ", c)
+tempVar = b
+b = c
+c = d
+print ("after swipping, the value of B is : ",b," , the value of C is : ",c)
+
+
+
